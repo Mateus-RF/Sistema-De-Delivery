0040909e24144035c87ee622edd3d11f55c33100
Lupercio <lupercionetof@gmail.com>
2025-09-22 18:56:33 -0300

    feat: Criar modelo e endpoints para listar restaurantes

--- app/models.py
+++ app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+# Usuário (Cliente ou Restaurante) - Task 7 
+class Usuario(AbstractUser):
+    TIPOS = (
+        ('cliente', 'Cliente'),
+        ('restaurante', 'Restaurante'),
+    )
+    nome = models.CharField(max_length=100)
+    tipo = models.CharField(max_length=20, choices=TIPOS)
+
+    REQUIRED_FIELDS = ['email', 'nome', 'tipo']
+
+    def __str__(self):
+        return self.nome
--- app/views.py
+++ app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# Task 7
+from rest_framework import viewsets
+from .models import Usuario
+from .serializers import UsuarioSerializer
+
+class RestauranteViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Usuario.objects.filter(tipo='restaurante')
+    serializer_class = UsuarioSerializer
